897b7ce50d8154978ec5fbfc4b6dc1bcf0f2f0cb
vasu <vasucp1207@gmail.com>
2025-01-15 21:53:10 +0530

    invoke gcc and execute binary with zig build

--- build.zig
+++ build.zig
@@ -17,13 +17,18 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
+    const invoke_gcc = b.step("gcc", "Compile with gcc");
+    invoke_gcc.makeFn = invokeGcc;
+
+    const exec_output = b.step("exec", "Execute output");
+    exec_output.makeFn = execOutput;
+
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
@@ -44,4 +49,31 @@ pub fn build(b: *std.Build) void {
     const coverage_step = b.step("coverage", "Tests coverage");
     test_step.dependOn(&run_exe_unit_tests.step);
     coverage_step.dependOn(&run_integation_tests.step);
+}
+
+fn invokeGcc(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
+    _ = step;
+    _ = prog_node;
+    const alloc = std.heap.page_allocator;
+    const argv = [_][]const u8{
+        "gcc",
+        "-o",
+        "output",
+        "output.c",
+    };
+    var child = std.process.Child.init(&argv, alloc);
+    try child.spawn();
+    _ = try child.wait();
+}
+
+fn execOutput(step: *std.Build.Step, prog_node: std.Progress.Node) !void {
+    _ = step;
+    _ = prog_node;
+    const alloc = std.heap.page_allocator;
+    const argv = [_][]const u8{
+        "./output",
+    };
+    var child = std.process.Child.init(&argv, alloc);
+    try child.spawn();
+    _ = try child.wait();
 }
